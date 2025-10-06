edc55cb3e33b31a8846cfc9a1b35b0aec9700ddd
taulur <matvey.chubarkov@gmail.com>
2025-10-06 15:52:40 +0500

    Добавьте файлы проекта.

---
+++ TRPO_pr7/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TRPO_pr7
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private User CurrentUser = new User();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = CurrentUser;
+        }
+
+        private void ResetUser(object sender, RoutedEventArgs e)
+        {
+            var currentUser = (User)Resources["CurrentUser"];
+            currentUser.Id = 0;
+            currentUser.Name = "Имя пользователя";
+            currentUser.Email = "user@example.com";
+            currentUser.Age = 18;
+        }
+    }
+}
